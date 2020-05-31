0884fd12e995a75a8f7f9553dc1200f86ef9bafe
Vishal Lama (164751) <vishlama@amdocs.com>
2020-05-31 20:02:34 +0530

    Project Skeleton + Adapting Jena TDB dependency

---
+++ src/main/java/ai/knowledgegraph/expertsystem/ExpertsystemApplication.java
@@ -0,0 +1,13 @@
+package ai.knowledgegraph.expertsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ai/knowledgegraph/expertsystem/ExpertsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.knowledgegraph.expertsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
